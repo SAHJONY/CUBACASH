63da962f15e4acbaf7c5f05566a6df4120eb8f0a
SAHJONY CAPITAL LLC. <sahjonyllc@outlook.com>
2026-09-14 20:24:38 -0500

    Add Spanish quick-start route alias

---
+++ app/[locale]/guia-rapida/page.tsx
@@ -0,0 +1,6 @@
+import {permanentRedirect} from 'next/navigation';
+
+export default async function GuiaRapidaPage({params}:{params:Promise<{locale:string}>}){
+  const {locale}=await params;
+  permanentRedirect(`/${locale}/quick-start`);
+}
